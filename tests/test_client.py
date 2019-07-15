a2808e406e88019384eced443d79ee328bc88d24
Stephen Hutchinson <stephenh@startmail.com>
2019-07-15 14:13:08 +0200

    Add pytest class

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,9 @@
+class TestFortimailClient(object):
 
-def func(x):
-    return x + 2
+    def test_one(self):
+        x = "this"
+        assert 'h' in x
 
-
-def test_answer():
-    assert func(3) == 5
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, 'startswith')
