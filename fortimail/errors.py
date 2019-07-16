73f8a5465e9c195eacacdffaf422e6310dd2f8cf
Stephen Hutchinson <stephenh@startmail.com>
2019-07-16 08:13:14 +0200

    Add root exception

--- fortimail/errors.py
+++ fortimail/errors.py
@@ -1,6 +1,26 @@
 '''
 Errors for the Fortimail client
+
+Having a root exception lets consumers of your API catch exceptions you raise on purpose. 
 '''
 
-class IllegalArgumentError(ValueError):
+class FortiMailError(Exception):
+    '''
+    Base-class for all exceptions raised by this module.
+    '''
+    def __init__(self, message=None, errors=None):
+        if errors:
+            message = ', '.join(errors)
+
+        self.errors = errors
+
+        super().__init__(message)
+
+class IllegalArgumentError(FortiMailError):
+    '''
+    Error raised when a function gets an illegal argument
+    
+    Arguments:
+        FortiMailError {Exception} -- The root exception
+    '''
     pass
